1c93fbd4d07896625f2f60c2737c712b0b075782
Patrice Lebel <plebel@o2web.ca>
2015-11-25 18:27:10 -0500

    shipping label improved

--- app/models/spree/shipping_rate_decorator.rb
+++ app/models/spree/shipping_rate_decorator.rb
@@ -12,10 +12,6 @@ Spree::ShippingRate.class_eval do
 
     return price if tax_rate.nil? || tax_amount == 0
 
-    Spree.t tax_rate.included_in_price? ? :including_tax : :excluding_tax,
-      scope: "shipping_rates.display_price",
-      price: price,
-      tax_amount: '',
-      tax_rate_name: 'taxes'
+    "#{price} (+ taxes)"
   end
 end
